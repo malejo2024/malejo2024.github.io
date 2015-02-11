40f19db3a7a6f2206ca2a112c271b4a5d649573b
Neftali Yagua <neftali.yagua@gmail.com>
2015-02-11 02:30:10 -0430

    Detector español

--- js/Detector.js
+++ js/Detector.js
@@ -26,13 +26,9 @@ Detector = {
 		if ( ! this.webgl ) {
 
 			domElement.innerHTML = window.WebGLRenderingContext ? [
-				'Sorry, your graphics card doesn\'t support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a>'
+				'Lo siento pero tu tarjeta de video no soporta WebGL'
 			].join( '\n' ) : [
-				'Sorry, your browser doesn\'t support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation">WebGL</a><br/>',
-				'Please try with',
-				'<a href="http://www.google.com/chrome">Chrome 10</a>, ',
-				'<a href="http://www.mozilla.com/en-US/firefox/all-beta.html">Firefox 4</a> or',
-				'<a href="http://nightly.webkit.org/">Safari 6</a>'
+				'Lo siento pero tu navegador no soporta WebGL'
 			].join( '\n' );
 
 		}
